5c85c0be62b2d1a03b89390ec513073d258fd791
Shayan <shayantrixytb@gmail.com>
2026-06-13 17:43:26 +0200

    Added: insertRandomCharacter \func

--- src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
+++ src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
@@ -4,6 +4,8 @@ import kotlin.random.Random
 import kotlin.text.count
 import kotlin.text.deleteAt
 
+
+
 /**
  * Character-level mutators from The Fuzzing Book's MutationFuzzer
  * (<https://www.fuzzingbook.org/html/MutationFuzzer.html>), plus [repeatRandomCharacter]. Each
@@ -28,8 +30,21 @@ internal object Mutators {
         //just had to use StringBuilder class
     }
     /** Inserts one character drawn uniformly from [alphabet] at a random position. */
-    fun insertRandomCharacter(input: String, alphabet: List<Char>, random: Random): String =
-        TODO("Exercise 1: insert one character from the alphabet at a random position")
+    fun insertRandomCharacter(input: String, alphabet: List<Char>, random: Random): String{
+        //"Exercise 1: insert one character from the alphabet at a random position")
+        val len = input.count()
+        // have a random number in the range of len
+        val rnInt = random.nextInt(0, len+1)
+        // with deleteAt we can delete at specific index
+
+        //make StringBuilder object to access deleteAt(index) func
+        val newString = StringBuilder(input)
+
+        val lenAlphabet = alphabet.count()
+        val randAlphabet = random.nextInt(0, lenAlphabet)
+        newString.insert(rnInt, alphabet[randAlphabet])
+        return newString.toString()
+    }
 
     /** Flips one randomly chosen low bit of one randomly chosen character. */
     fun flipRandomCharacter(input: String, random: Random): String =
